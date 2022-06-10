749cb7fc8b8ef38045aa382c67b849f91368d84a
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-10 14:48:22 +0700

    Agency Class;

--- app/src/main/java/com/example/oopproject/classes/Agency.java
+++ app/src/main/java/com/example/oopproject/classes/Agency.java
@@ -1,4 +1,9 @@
 package com.example.oopproject.classes;
 
 public class Agency {
+    private String id;
+    private String DC_id;
+    private String openedTime;
+    private String closedTime;
+    private String phoneNumber;
 }
