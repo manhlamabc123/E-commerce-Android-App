4d263b3bddecd68159eac4bc38b94c37291c760e
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-10 14:30:47 +0700

    ProductColor Class

--- app/src/main/java/com/example/oopproject/classes/ProductColor.java
+++ app/src/main/java/com/example/oopproject/classes/ProductColor.java
@@ -1,4 +1,8 @@
 package com.example.oopproject.classes;
 
 public class ProductColor {
+    private String id;
+    private String phone_id;
+    private String color_id;
+    private double priceBuy;
 }
