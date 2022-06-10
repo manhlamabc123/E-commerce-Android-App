432811d256fa45872d13c5b6f5be1d408017289c
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-10 14:45:46 +0700

    Customer Class

--- app/src/main/java/com/example/oopproject/classes/Customer.java
+++ app/src/main/java/com/example/oopproject/classes/Customer.java
@@ -1,4 +1,12 @@
 package com.example.oopproject.classes;
 
+import java.util.Date;
+
 public class Customer {
+    private String id;
+    private String fullName;
+    private Date dateOfBirth;
+    private String gender;
+    private String DC_id;
+    private String phoneNumber;
 }
