f59a3c64edd734f46678b20b0e6f990031c45696
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-10 14:42:21 +0700

    Order Class

--- app/src/main/java/com/example/oopproject/classes/Order.java
+++ app/src/main/java/com/example/oopproject/classes/Order.java
@@ -1,4 +1,12 @@
 package com.example.oopproject.classes;
 
+import java.util.Date;
+
 public class Order {
+    private String id;
+    private String KH_id;
+    private String CN_id;
+    private String NV_id;
+    private Date buyDate;
+    private String purchaseFrom;
 }
