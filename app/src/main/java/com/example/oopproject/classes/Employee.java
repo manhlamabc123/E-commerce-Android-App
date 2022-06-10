45e5e7ea8b463975b2c08fb04a6fde899773473c
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-10 14:49:27 +0700

    Employee Class

--- app/src/main/java/com/example/oopproject/classes/Employee.java
+++ app/src/main/java/com/example/oopproject/classes/Employee.java
@@ -1,4 +1,9 @@
 package com.example.oopproject.classes;
 
 public class Employee {
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private String CN_id;
+    private String job;
 }
