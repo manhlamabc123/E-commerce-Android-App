997575c5f48351eaf5b9a2a755d63009f39a312c
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-10 14:35:39 +0700

    Product Class

--- app/src/main/java/com/example/oopproject/classes/Product.java
+++ app/src/main/java/com/example/oopproject/classes/Product.java
@@ -1,4 +1,16 @@
 package com.example.oopproject.classes;
 
 public class Product {
+    private String id;
+    private String name;
+    private String category;
+    private double priceImport;
+    private String manufacturer;
+    private String os;
+    private String include;
+    private double ram;
+    private double memory;
+    private double screen;
+    private String description;
+    private double warranty;
 }
