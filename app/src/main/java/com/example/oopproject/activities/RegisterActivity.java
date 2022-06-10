b0316e67457c8eb122d058c6b348c2dd791ae6bf
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-10 14:23:42 +0700

    Move Activity to activities

--- app/src/main/java/com/example/oopproject/activities/RegisterActivity.java
+++ app/src/main/java/com/example/oopproject/activities/RegisterActivity.java
@@ -1,8 +1,10 @@
-package com.example.oopproject;
+package com.example.oopproject.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.oopproject.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
