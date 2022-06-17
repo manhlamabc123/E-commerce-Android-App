cac7f4e4cf6b4c6e856669b65704adb3ddce5fb2
mhai247 <minhhai24072001@gmail.com>
2022-06-17 19:48:32 +0700

    Verify Register and Login

--- app/src/main/java/com/example/oopproject/LoginActivity.java
+++ app/src/main/java/com/example/oopproject/LoginActivity.java
@@ -1,14 +1,90 @@
 package com.example.oopproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText InputNumber, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+
+    private String parentDbName = "Users";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        LoginButton = (Button) findViewById(R.id.register_btn);
+        InputPassword = (EditText) findViewById(R.id.register_password_input);
+        InputNumber = (EditText) findViewById(R.id.register_phone_number_input);
+        loadingBar = new ProgressDialog(this);
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LoginUser();
+            }
+        });
     }
-}
+
+    private void LoginUser() {
+        String phone = InputNumber.getText().toString();
+        String password = InputPassword.getText().toString();
+
+        if (TextUtils.isEmpty(phone))
+        {
+            Toast.makeText(this, "Please write your phone...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait, we are checking credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+        }
+
+//        AllowAccessToAccount(phone, password);
+    }
+
+//    private void AllowAccessToAccount(String phone, String password) {
+//        final DatabaseReference RootRef;
+//        RootRef = FirebaseDatabase.getInstance().getReference();
+//
+//        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                if (snapshot.child(parentDbName).child(phone).exists()) {
+//
+//                }
+//                else {
+//                    Toast.makeText(LoginActivity.this, "Account with this " + phone + " not exists.", Toast.LENGTH_SHORT).show();
+//                    loadingBar.dismiss();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        }
+    }
