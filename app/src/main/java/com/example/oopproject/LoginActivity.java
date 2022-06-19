939f06a8c23f3eed2ea83098d44592fd4ec7b760
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-19 16:19:08 +0700

    Comment error :v

--- app/src/main/java/com/example/oopproject/LoginActivity.java
+++ app/src/main/java/com/example/oopproject/LoginActivity.java
@@ -11,11 +11,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+//import com.google.firebase.database.DataSnapshot;
+//import com.google.firebase.database.DatabaseError;
+//import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/oopproject/MainActivity.java
+++ app/src/main/java/com/example/oopproject/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.firebase.database.DatabaseReference;
+//import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
     private Button joinNowButton, loginButton;
