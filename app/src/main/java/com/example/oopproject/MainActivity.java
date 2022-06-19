630e05f558cf3ab062064aa371098117baa3241c
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-19 16:31:14 +0700

    App Function: skip Login

--- app/src/main/java/com/example/oopproject/MainActivity.java
+++ app/src/main/java/com/example/oopproject/MainActivity.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 //import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
     private Button joinNowButton, loginButton;
+    private TextView skipLoginText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
         joinNowButton = (Button)findViewById((R.id.main_join_now_btn));
         loginButton = (Button)findViewById(R.id.main_login_btn);
-
+        skipLoginText = (TextView)findViewById(R.id.skip_login_text);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +36,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        skipLoginText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
                 startActivity(intent);
             }
         });
