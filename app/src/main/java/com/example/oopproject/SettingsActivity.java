461cebd8ee4e513d8b1fd90990dfcef7d334014f
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-20 07:34:00 +0700

    Ignore Gradle Build

--- app/src/main/java/com/example/oopproject/SettingsActivity.java
+++ app/src/main/java/com/example/oopproject/SettingsActivity.java
@@ -2,13 +2,45 @@ package com.example.oopproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private CircleImageView profileImageView;
+    private EditText fullNameEditText, userPhoneEditText, addressEditText;
+    private TextView profileChangeTextBtn,  closeTextBtn, saveTextButton;
+
+    private Uri imageUri;
+    private String myUrl = "";
+    private StorageTask uploadTask;
+    private StorageReference storageProfilePictureRef;
+    private String checker = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+//        storageProfilePictureRef = FirebaseStorage.getInstance().getReference()
+
+        profileImageView = (CircleImageView) findViewById(R.id.settings_profile_image);
+        fullNameEditText = (EditText) findViewById(R.id.settings_name);
+        userPhoneEditText = (EditText) findViewById(R.id.settings_phone_number);
+
+        Spinner spinner = (Spinner) findViewById(R.id.gender_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
