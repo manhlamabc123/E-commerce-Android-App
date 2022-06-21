400d35f51b05fb8c5525e080b5ef2a33929154f0
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-06-21 12:15:29 +0700

    Update Product Class

--- app/src/main/java/com/example/oopproject/classes/Product.java
+++ app/src/main/java/com/example/oopproject/classes/Product.java
@@ -13,4 +13,56 @@ public class Product {
     private double screen;
     private String description;
     private double warranty;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPriceImport() {
+        return priceImport;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public String getInclude() {
+        return include;
+    }
+
+    public double getRam() {
+        return ram;
+    }
+
+    public double getMemory() {
+        return memory;
+    }
+
+    public double getScreen() {
+        return screen;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getWarranty() {
+        return warranty;
+    }
+
+    public double getPrice() {
+        return 0;
+    }
 }
