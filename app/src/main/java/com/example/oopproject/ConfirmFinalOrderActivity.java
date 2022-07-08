45ab49b8d7ba37d14a17cca873a5aea33695b098
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-07-08 11:14:58 +0700

    Confirm Final Order Activity

--- app/src/main/java/com/example/oopproject/ConfirmFinalOrderActivity.java
+++ app/src/main/java/com/example/oopproject/ConfirmFinalOrderActivity.java
@@ -3,12 +3,21 @@ package com.example.oopproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class ConfirmFinalOrderActivity extends AppCompatActivity {
+
+    private Spinner paymentMethodSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm_final_order);
+
+        paymentMethodSpinner = (Spinner) findViewById(R.id.payment_method_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(ConfirmFinalOrderActivity.this, R.array.payment_methods, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        paymentMethodSpinner.setAdapter(adapter);
     }
 }
