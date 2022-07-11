2f4ea192f1bc3cf6d0f189cab15c8b2fb895900d
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-07-11 18:53:46 +0700

    Create Order Detail

---
+++ app/src/main/java/com/example/oopproject/OderDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.oopproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OderDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oder_details);
+    }
+}
